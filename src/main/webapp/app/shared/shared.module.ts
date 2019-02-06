e5a9f9874aaa020c3d2cd84f01c7b068f2916f9d
Vishal Patidar <vishalpatidar00789@gmail.com>
2019-02-06 16:07:13 +0530

    material integration by vishal

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -3,13 +3,14 @@ import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 
 import { NgbDateMomentAdapter } from './util/datepicker-adapter';
 import { IdbSharedLibsModule, IdbSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
+import { JhMaterialModule } from 'app/app-material.module';
 
 @NgModule({
-    imports: [IdbSharedLibsModule, IdbSharedCommonModule],
+    imports: [IdbSharedLibsModule, IdbSharedCommonModule, JhMaterialModule],
     declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
     providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
     entryComponents: [JhiLoginModalComponent],
-    exports: [IdbSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
+    exports: [IdbSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective, JhMaterialModule],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class IdbSharedModule {
