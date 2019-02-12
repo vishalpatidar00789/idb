6d27fa3ce1e7b156e6efca3e4a8fd7f6f0729ec3
Lokendra <Lokendra@192.168.0.5>
2019-02-12 23:39:52 +0530

    Bootstrap landing form

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
-
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { LoginModalService, AccountService, Account } from 'app/core';
 
 @Component({
@@ -12,18 +12,32 @@ import { LoginModalService, AccountService, Account } from 'app/core';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
+    employeeForm:  FormGroup;
 
     constructor(
         private accountService: AccountService,
         private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
-    ) {}
+        private eventManager: JhiEventManager,
+        private fb: FormBuilder) {}
 
     ngOnInit() {
         this.accountService.identity().then((account: Account) => {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
+
+        this.employeeForm = this.fb.group({
+            gender : ['', Validators.required],
+            lfGender : ['', Validators.required],
+            age : ['', Validators.required],
+            country : ['', Validators.required],
+            state : ['', Validators.required],
+            city : ['', Validators.required]
+          });
+
+          this.employeeForm.valueChanges.subscribe(data => {
+            this.logValidationErrors(this.employeeForm);
+          });
     }
 
     registerAuthenticationSuccess() {
@@ -41,4 +55,60 @@ export class HomeComponent implements OnInit {
     login() {
         this.modalRef = this.loginModalService.open();
     }
+
+    logValidationErrors(group: FormGroup = this.employeeForm): void {
+        Object.keys(group.controls).forEach((key: string) => {
+          const abstractControl = group.get(key);
+
+          if(abstractControl instanceof FormGroup) {
+            this.logValidationErrors(abstractControl);
+          } else {
+            this.formErrors[key] = '';
+            if(abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
+              const messages  = this.validationMessages[key];
+              console.log(messages);
+              for(const errorKey in abstractControl.errors) {
+                if(errorKey) {
+                  this.formErrors[key] += messages[errorKey] + ' ';
+                }
+              }
+            }
+          }
+        });
+    }
+
+    validationMessages = {
+        'gender' : {
+          'required' : 'Gender is required',
+        },
+        'lfGender' : {
+            'required' : 'Looking for gender is required',
+        },
+        'age' : {
+        'required' : 'Age is required',
+        },
+        'country' : {
+            'required' : 'Country is required',
+        },
+        'state' : {
+            'required' : 'State is required',
+        },
+        'city' : {
+            'required' : 'City is required',
+        }
+
+    };
+
+    formErrors = {
+      'gender' : '',
+      'lfGender' : '',
+      'age' : '',
+      'country' : '',
+      'state' : '',
+      'city' : ''
+    };
+
+    onSubmit(): void {
+        console.log(this.employeeForm.value);
+      }
 }
--- src/main/webapp/app/shared/shared-libs.module.ts
+++ src/main/webapp/app/shared/shared-libs.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgJhipsterModule } from 'ng-jhipster';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { CookieModule } from 'ngx-cookie';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     imports: [NgbModule.forRoot(), InfiniteScrollModule, CookieModule.forRoot(), FontAwesomeModule],
-    exports: [FormsModule, CommonModule, NgbModule, NgJhipsterModule, InfiniteScrollModule, FontAwesomeModule]
+    exports: [FormsModule, ReactiveFormsModule, BrowserAnimationsModule, CommonModule, NgbModule, NgJhipsterModule, InfiniteScrollModule, FontAwesomeModule]
 })
 export class IdbSharedLibsModule {
     static forRoot() {
